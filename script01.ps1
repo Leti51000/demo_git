894113e8bf8c8701937c5dfe6e62ace4e230f0ef
Leti51000 <65112823+Leti51000@users.noreply.github.com>
2020-05-10 16:11:28 +0200

    Création de l'utilisateur, avec son mail et mdp
    Mdp force/No expire/ No change

--- script01.ps1
+++ script01.ps1
@@ -5,3 +5,5 @@ $nom = Read-Host "Merci de Rentrer le Nom et le Prénom de l'Utilisateur à Cré
 $login = Read-Host "Merci de Rentrer le login de l'Utilisateur à Créer"
 
 $mdp = Read-Host "Merci de Rentrer le Mot de Passe de l'Utilisateur à Créer"
+
+New-ADUser -Name $nom -SamAccountName $login -UserPrincipalName $login@acme.fr -AccountPassword (ConvertTo-SecureString -AsPlainText $mdp -Force) -PasswordNeverExpires $true -CannotChangePassword $true -Enabled $true
