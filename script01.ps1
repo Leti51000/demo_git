23ca2a690aca854361bb9ecdd1a4a4a8cf5b8256
Leti51000 <65112823+Leti51000@users.noreply.github.com>
2020-05-10 16:07:57 +0200

    Ajout du nom_login_mdp

--- script01.ps1
+++ script01.ps1
@@ -1,1 +1,7 @@
 #Création d'un nouvel utilisateur et de son dossier partagé à son nom
+
+$nom = Read-Host "Merci de Rentrer le Nom et le Prénom de l'Utilisateur à Créer"
+
+$login = Read-Host "Merci de Rentrer le login de l'Utilisateur à Créer"
+
+$mdp = Read-Host "Merci de Rentrer le Mot de Passe de l'Utilisateur à Créer"
